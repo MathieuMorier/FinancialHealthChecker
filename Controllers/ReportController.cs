24fb4434be20f52675f9833f2c14aea9c4a6d408
MathieuMorier <mathieu270813@gmail.com>
2023-11-28 17:49:17 -0500

    AI integratiion

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.Managers;
+using OpenAI;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.Interfaces;
+using OpenAI.ObjectModels.ResponseModels;
+
+namespace FinancialHealthChecker.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly OpenAIService openAiService;
+
+        public ReportController(OpenAIService openAIService)
+        {
+            this.openAiService = openAIService;
+        }
+
+        public async Task<IActionResult> Report()
+        {
+            var aiResponse = await getOpenAIResponse();
+            var stringResponse = aiResponse.Choices.First().Text;
+            ViewData["OpenAI"] = stringResponse;
+            return View();
+        }
+
+        private async Task<CompletionCreateResponse> getOpenAIResponse()
+        {
+            var completionResult = await openAiService.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = "Explain quantum mechanics",
+                Model = "gpt-3.5-turbo-instruct",
+                MaxTokens = 250
+            });
+
+            return completionResult;
+        }
+    }
+}
