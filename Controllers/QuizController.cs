6b6893fd702ae7fc9591c93f0a84f66834ebeb9a
MathieuMorier <mathieu270813@gmail.com>
2023-12-02 16:09:00 -0500

    Homepage done

---
+++ Controllers/QuizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialHealthChecker.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Quiz()
+        {
+            return View();
+        }
+    }
+}
